8cd1b756f9f6399c6dd2b8b37244c1cf0319cacf
Igoche Anthony <54316737+OdehIgoche@users.noreply.github.com>
2024-12-05 15:58:11 +0100

    index page animation

--- js/script.js
+++ js/script.js
@@ -20,10 +20,65 @@ gsap.from(menu_items.children ,{
     }
 })
 
-gsap.from('.section-1',{
+gsap.from('.hero-video',{
     opacity:0,
     delay:2,
     duration:3,
     y:50,
     scrollTrigger: '.section-1'
+})
+
+gsap.from('.section-2',{
+    opacity:0,
+    duration:1.5,
+    y:50,
+    scrollTrigger: '.section-2'
+})
+
+gsap.from('.section-3',{
+    opacity:0,
+    duration:1.5,
+    y:50,
+    scrollTrigger: '.section-3'
+})
+
+gsap.from('.section-4',{
+    opacity:0,
+    duration:1.5,
+    y:50,
+    scrollTrigger: '.section-4'
+})
+
+gsap.from('.section-5',{
+    opacity:0,
+    duration:1.5,
+    y:50,
+    scrollTrigger: '.section-5'
+})
+
+gsap.from('.section-6',{
+    opacity:0,
+    duration:1.5,
+    y:50,
+    scrollTrigger: '.section-6'
+})
+
+gsap.from('.section-7',{
+    opacity:0,
+    duration:1.5,
+    y:50,
+    scrollTrigger: '.section-7'
+})
+
+gsap.from('.section-8',{
+    opacity:0,
+    duration:1.5,
+    y:50,
+    scrollTrigger: '.section-8'
+})
+gsap.from('.footer',{
+    opacity:0,
+    duration:1.5,
+    y:50,
+    scrollTrigger: '.footer'
 })
